b68df29ead026e2b884532d280dd5eaf6f7c7892
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-06 15:52:49 +0300

    commit tree 5f36249eb30c6b6a1da4acc79eac5d0a490cd2c1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+// Alternate Solution
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+// Solution
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0, 1)}!`;
